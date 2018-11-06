fe7c9f180609ffe6f0e2d0e23225d25750da8177
JamesLin1996 <32826962+JamesLin1996@users.noreply.github.com>
2018-11-06 16:39:02 -0600

    Add files via upload

---
+++ player.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include "deck.h"
+#include "player.h"
+
+Player::Player() {
+    myName = '\0';
+}
+
+Player::~Player() {
+    //The destructor
+}
