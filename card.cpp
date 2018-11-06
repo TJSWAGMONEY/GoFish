d18c81d058955fdab27e48d49f35b979fff712f1
Troy Jackson <troyjax@att.net>
2018-11-05 20:20:06 -0600

    Half of card.cpp functions
    getRank, sameSuitAs, suitString, and the overloaded operators

--- card.cpp
+++ card.cpp
@@ -20,3 +20,63 @@ string toString() {
 
 }
 
+int getRank() const {
+
+    return myRank;
+
+}
+
+bool sameSuitAs(const Card& c) const {
+
+    if(c.mySuit == this.mySuit) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+
+string suitString(Suit s) const {
+
+    if(s == spades) {
+        return "s";
+    }
+    else if(s == hearts) {
+        return "h";
+    }
+    else if(s == diamonds) {
+        return "d";
+    }
+    else if(s == clubs) {
+        return "c";
+    }
+    else {
+        cout << "The suitString method is written incorrectly." << endl;
+        return "error";
+    }
+
+}
+
+bool operator == (const Card& rhs) const {
+
+    if((rhs.mySuit == this.mySuit) && (rhs.myRank == this.myRank)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+
+bool operator != (const Card& rhs) const {
+
+    if((rhs.mySuit == this.mySuit) && (rhs.myRank == this.myRank)) {
+        return false;
+    }
+    else {
+        return true;
+    }
+
+}
+
