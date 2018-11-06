b05a01c628aff3a70ed316d4d6eb59b4f1d17992
JamesLin1996 <32826962+JamesLin1996@users.noreply.github.com>
2018-11-05 20:28:01 -0600

    Update card.cpp

--- card.cpp
+++ card.cpp
@@ -12,21 +12,29 @@ using namespace std;
 //default constructor
 Card::Card() {
     myRank = 1;
+    mySuit = spades;
 }
 
-string toString() {
-
-    //
-
+//overloaded constructor, sets rank and suit
+Card::Card(int rank, Suit s) {
+    this->myRank = rank;
+    this->mySuit = s;
 }
 
-int getRank() const {
+//method returns card as a string
+string Card::toString() const {
+    string rank = rankString(myRank);
+    string suit = suitString(mySuit);
+    return rank+suit;
+}
+
+int Card::getRank() const {
 
     return myRank;
 
 }
 
-bool sameSuitAs(const Card& c) const {
+bool Card::sameSuitAs(const Card& c) const {
 
     if(c.mySuit == this.mySuit) {
         return true;
@@ -37,7 +45,14 @@ bool sameSuitAs(const Card& c) const {
 
 }
 
-string suitString(Suit s) const {
+//returns rank of card as string
+string Card::rankString(int r) const {
+    //array of card ranks
+    string myCards[][1] = {"/0", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    return myCards[0][r];
+}
+
+string Card::suitString(Suit s) const {
 
     if(s == spades) {
         return "s";
