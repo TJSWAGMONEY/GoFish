708631d0e19da0560530948a1dbe59e16bddee59
Troy Jackson <troyjax@att.net>
2018-11-06 16:13:19 -0600

    Update deck.cpp

--- deck.cpp
+++ deck.cpp
@@ -7,3 +7,33 @@
 #include <algorithm>
 #include "deck.h"
 
+using namespace std;
+
+Card Deck::dealCard() {
+
+    if(size() == 0) {
+        return myCards[0];//This should never actually occur, because
+                          //players cannot deal from an empty deck.
+    }
+    else {
+        Card newCard = myCards[myIndex];
+        myIndex--;
+        return newCard;
+    }
+
+}
+
+int Deck::size() const {
+
+    return(52-myIndex);
+
+}
+
+void Deck::shuffle() {
+
+    //
+
+}
+
+
+
