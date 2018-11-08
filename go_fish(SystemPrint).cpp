d9f60347be31225df4435c62cfbff330cfa70e36
JamesLin1996 <32826962+JamesLin1996@users.noreply.github.com>
2018-11-07 19:45:36 -0600

    Update and rename go_fish(BeforePrintStatements).cpp to go_fish(SystemPrint).cpp

--- go_fish(SystemPrint).cpp
+++ go_fish(SystemPrint).cpp
@@ -24,8 +24,14 @@ int main() {
     Deck d;
     d.shuffle();
 
+    cout << "-----------Dealing Cards to Players-----------" << endl;
     dealHand(d, p1, numCards);
     dealHand(d, p2, numCards);
+
+    cout << p1.getName() << "' Hand: " << p1.showHand() << endl;
+    cout << p1.getName() << "' Books: " << p1.showBooks() << endl;
+    cout << p2.getName() << "'s Hand: " << p2.showHand() << endl;
+    cout << p2.getName() << "'s Books: " << p2.showBooks() << endl;
 
     Card* temp1 = new Card;
     Card* temp2 = new Card;
@@ -33,39 +39,57 @@ int main() {
     bool p1turn = true;
     bool p2turn = false;
 
+    cout << "\n-----------Initial Check for Books-----------" << endl;
+    cout << p1.getName() << " lays down his books.\n";
     while(p1.checkForBook(temp1, temp2)) {
         p1.bookCards(*temp1, *temp2);
         p1.removeCardFromHand(*temp1);
         p1.removeCardFromHand(*temp2);
     }
+    cout << p1.getName() << "' Hand: " << p1.showHand() << endl;
+    cout << p1.getName() << "' Books: " << p1.showBooks() << "\n" << endl;
+
+    cout << p2.getName() << " lays down his books.\n";
     while(p2.checkForBook(temp1, temp2)) {
         p2.bookCards(*temp1, *temp2);
         p2.removeCardFromHand(*temp1);
         p2.removeCardFromHand(*temp2);
     }
 
+    cout << p2.getName() << "'s Hand: " << p2.showHand() << endl;
+    cout << p2.getName() << "'s Books: " << p2.showBooks() << endl;
+
+    cout << "\n-----------Game Mechanics Begin-----------" << endl;
     while(winCon != 26) {
 
         if(p1turn) {
             if((p1.getHandSize()) > 0) {
                 *(temp1) = p1.chooseCardFromHand();
+                cout << p1.getName() << " asks - Do you have a " << temp1->getRank() << endl;
+
                 if(p2.cardInHand(*temp1)) {
+                    cout << p2.getName() << " says - Yes. I have a " << temp1->getRank() << endl;
                     while(p2.cardInHand(*temp1)) {
                         p1.addCard(p2.removeCardFromHand(*temp1));
                     }
                 }
                 else {
                     if((d.size()) > 0) {
-                        p1.addCard(d.dealCard());
+                        cout << p2.getName() << " says - Go Fish" << endl;
+                        *temp2 = d.dealCard();
+                        p1.addCard(*temp2);
+                        cout << p1.getName() << " draws " << *temp2 << endl;
                     }
                 }
             }
             else {
+                cout << p1.getName() << " is out of cards and draws a card from the deck\n";
                 if((d.size()) > 0) {
                     p1.addCard(d.dealCard());
                 }
             }
 
+            cout << p1.getName() << " books the " << temp1->getRank() << "..\n";
             while(p1.checkForBook(temp1, temp2)) {
                 p1.bookCards(*temp1, *temp2);
                 p1.removeCardFromHand(*temp1);
@@ -80,24 +104,31 @@ int main() {
         else if(p2turn) {
             if((p2.getHandSize()) > 0) {
                 *(temp2) = p2.chooseCardFromHand();
+                cout << p2.getName() << " asks - Do you have a " << temp2->getRank() << endl;
                 if(p1.cardInHand(*temp2)) {
+                    cout << p1.getName() << " says - Yes. I have a " << temp2->getRank() << endl;
                     while(p1.cardInHand(*temp2)) {
                         p2.addCard(p1.removeCardFromHand(*temp2));
                     }
                 }
                 else {
                     if((d.size()) > 0) {
-                        p2.addCard(d.dealCard());
+                        cout << p1.getName() << " says - Go Fish" << endl;
+                        *temp1 = d.dealCard();
+                        p2.addCard(*temp1);
+                        cout << p2.getName() << " draws " << *temp1 <<  endl;
                     }
                 }
 
             }
             else {
+                cout << p2.getName() << " is out of cards and draws a card from the deck\n";
                 if((d.size()) > 0) {
                     p2.addCard(d.dealCard());
                 }
             }
 
+            cout << p2.getName() << " books the " << temp2->getRank() << "..\n";
             while(p2.checkForBook(temp1, temp2)) {
                 p2.bookCards(*temp1, *temp2);
                 p2.removeCardFromHand(*temp1);
@@ -109,15 +140,24 @@ int main() {
 
         }
 
+        cout << "\n" << p1.getName() << "' Hand: " << p1.showHand() << endl;
+        cout << p1.getName() << "' Books: " << p1.showBooks() << endl;
+        cout << p2.getName() << "'s Hand: " << p2.showHand() << endl;
+        cout << p2.getName() << "'s Books: " << p2.showBooks() << "\n" << endl;
+
         winCon = (p1.getBookSize() + p2.getBookSize()) / 2;
 
     }
 
+    cout << "Game is Finished!" << endl;
+
     if((p1.getBookSize()) > (p2.getBookSize())) {
-        //Declare p1 as the winner
+        cout << p1.getName() << " is the winner!!\n";
     }
     else if((p2.getBookSize()) > (p1.getBookSize())) {
-        //Declare p2 as the winner
+        cout << p2.getName() << " is the winner!!\n";
+    } else {
+        cout << "It's a Draw!!\n";
     }
 
     delete temp1;
@@ -130,4 +170,3 @@ void dealHand(Deck &d, Player &p, int numCards) {
     for (int i=0; i < numCards; i++)
         p.addCard(d.dealCard());
 }
-
